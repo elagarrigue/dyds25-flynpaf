655d3b2979fe50036db077737fccd263e7153f7f
UriNoHi <jrumachado@gmail.com>
2025-06-19 16:29:40 -0300

    Refactor en Test (todo mas bonito)

--- composeApp/src/desktopTest/kotlin/TestRepository.kt
+++ composeApp/src/desktopTest/kotlin/TestRepository.kt
@@ -24,13 +24,24 @@ class TestRepository {
         override fun cacheMovie(movie: Movie) { cache.add(movie) }
         override fun isEmpty(): Boolean { return cache.isEmpty() }
         override fun getMovieList(): List<Movie> { return cache }
-
     }
 
     class RemoteMoviesSourceImpl():RemoteMoviesSource {
-        val MovieExample1 = Movie(1, "Example title 1", "Example overview 1", "Example releaseDate 1", "Example poster 1", "Example backdrop 1", "Example originalTitle 1", "Example original Language 1", 0.0, 0.0)
-        val MovieExample2 = Movie(2, "Example title 2", "Example overview 2", "Example releaseDate 2", "Example poster 2", "Example backdrop 2", "Example originalTitle 2", "Example original Language 2", 0.0, 0.0)
-        val MovieExample3 = Movie(3, "Example title 3", "Example overview 3", "Example releaseDate 3", "Example poster 3", "Example backdrop 3", "Example originalTitle 3", "Example original Language 3", 0.0, 0.0)
+        val MovieExample1 = Movie(  1, "Example title 1",
+                                    "Example overview 1", "Example releaseDate 1",
+                                    "Example poster 1", "Example backdrop 1",
+                                    "Example originalTitle 1", "Example originalLanguage 1",
+                                    1.0, 1.0)
+        val MovieExample2 = Movie(  2, "Example title 2",
+                                    "Example overview 2", "Example releaseDate 2",
+                                    "Example poster 2", "Example backdrop 2",
+                                    "Example originalTitle 2", "Example originalLanguage 2",
+                                    2.0, 2.0)
+        val MovieExample3 = Movie(  3, "Example title 3",
+                                    "Example overview 3", "Example releaseDate 3",
+                                    "Example poster 3", "Example backdrop 3",
+                                    "Example originalTitle 3", "Example originalLanguage 3",
+                                    3.0, 3.0)
 
         override suspend fun getMovieByIdRemote(id: Int): Movie? {
             return when (id){
@@ -53,70 +64,72 @@ class TestRepository {
     val MovieExample1 = Movie(  1, "Example title 1",
                                 "Example overview 1", "Example releaseDate 1",
                                 "Example poster 1", "Example backdrop 1",
-                                "Example originalTitle 1", "Example original Language 1",
-                                0.0, 0.0)
+                                "Example originalTitle 1", "Example originalLanguage 1",
+                                1.0, 1.0)
     val MovieExample2 = Movie(  2, "Example title 2",
                                 "Example overview 2", "Example releaseDate 2",
                                 "Example poster 2", "Example backdrop 2",
-                                "Example originalTitle 2", "Example original Language 2",
-                                0.0, 0.0)
+                                "Example originalTitle 2", "Example originalLanguage 2",
+                                2.0, 2.0)
     val MovieExample3 = Movie(  3, "Example title 3",
                                 "Example overview 3", "Example releaseDate 3",
                                 "Example poster 3", "Example backdrop 3",
-                                "Example originalTitle 3", "Example original Language 3",
-                                0.0, 0.0)
+                                "Example originalTitle 3", "Example originalLanguage 3",
+                                3.0, 3.0)
 
     @BeforeTest
     fun setUp(){
+        val listOfPopularMovies = listOf(MovieExample1,MovieExample2,MovieExample3)
         localMoviesSourceFake = LocalMoviesSourceSourceImpl()
-            localMoviesSourceFake.initializeMovieCache(listOf(MovieExample1,MovieExample2,MovieExample3))
-        remoteMoviesSourceFake = RemoteMoviesSourceImpl()
-        moviesRepositoryFake = MoviesRepositoryImpl(localMoviesSourceFake, remoteMoviesSourceFake)
+        localMoviesSourceFake.initializeMovieCache(listOfPopularMovies)
 
+        remoteMoviesSourceFake = RemoteMoviesSourceImpl()
+
+        moviesRepositoryFake = MoviesRepositoryImpl(
+            localMoviesSourceFake,
+            remoteMoviesSourceFake
+        )
     }
 
     @Test
     fun `Test de getPopularMovies() Local`()= runTest{
         //arrange
-
+        val expected = listOf(MovieExample1,MovieExample2,MovieExample3)
         //act
         val result = moviesRepositoryFake.getPopularMovies()
-
         // assert
-        assertEquals(result,listOf(MovieExample1,MovieExample2,MovieExample3))
+        assertEquals(expected,result)
     }
 
     @Test
     fun `Test de getPopularMovies() Remoto`()= runTest{
         //arrange
         localMoviesSourceFake.initializeMovieCache(emptyList())
+        val expected = listOf(MovieExample1,MovieExample2,MovieExample3)
         //act
         val result = moviesRepositoryFake.getPopularMovies()
-
         // assert
-        assertEquals(result,listOf(MovieExample1,MovieExample2,MovieExample3))
+        assertEquals(expected, result,)
     }
 
     @Test
     fun `Test de getMovieById() Remoto`()= runTest{
         //arrange
-
+        val expected = MovieExample1
         //act
         val result = moviesRepositoryFake.getMovieDetailById(1)
-
         // assert
-        assertEquals(result,MovieExample1)
+        assertEquals(expected, result)
     }
 
     @Test
     fun `Test de getMovieById() Remoto id erroneo`()= runTest{
         //arrange
-
+        val expected = null
         //act
         val result = moviesRepositoryFake.getMovieDetailById(4)
-
         // assert
-        assertEquals(result,null)
+        assertEquals(expected, result)
     }
 
 
