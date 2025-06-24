9c3983a056e225a9dcd135694f264b25331dbd09
Micaela Cano <mica.709@gmail.com>
2025-06-24 13:54:04 -0300

    Creacion de la clase FakeMovieRepository

---
+++ composeApp/src/desktopTest/kotlin/edu/dyds/movies/domain/fakes/FakeMovieRepository.kt
@@ -0,0 +1,17 @@
+package edu.dyds.movies.domain.fakes
+
+import edu.dyds.movies.domain.entity.Movie
+import edu.dyds.movies.domain.repository.MoviesRepository
+
+class FakeMovieRepository (
+        private val fakeMovieList: List<Movie>
+) : MoviesRepository {
+
+        override suspend fun getMovieDetailById(id: Int): Movie? {
+            return fakeMovieList.find { it.id == id }
+        }
+
+        override suspend fun getPopularMovies(): List<Movie> {
+            return fakeMovieList
+        }
+}
