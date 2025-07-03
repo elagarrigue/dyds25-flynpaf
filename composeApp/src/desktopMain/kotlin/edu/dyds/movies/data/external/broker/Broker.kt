2004551ed056d0d4928091af73f3e5dfd347fc43
Micaela Cano <mica.709@gmail.com>
2025-07-03 17:07:35 -0300

    Creacion del Broker para tmdb y omdb

---
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/external/broker/Broker.kt
@@ -0,0 +1,46 @@
+package edu.dyds.movies.data.external.broker
+
+import edu.dyds.movies.data.external.MovieExternalSource
+import edu.dyds.movies.data.external.omdb.OMDBMovieExternalSource
+import edu.dyds.movies.data.external.tmdb.TMDBMovieExternalExternalSource
+import edu.dyds.movies.domain.entity.Movie
+
+
+class Broker (
+    private val tmdbMovieSource: TMDBMovieExternalExternalSource,
+    private val omdbMovieSource: OMDBMovieExternalSource
+) : MovieExternalSource {
+
+    override suspend fun getMovieByTitleRemote(title : String) : Movie? {
+        val tmdb = try {
+            tmdbMovieSource.getMovieByTitleRemote(title)
+        } catch (e: Exception) {
+            null
+        }
+
+        val omdb = try {
+            omdbMovieSource.getMovieByTitleRemote(title)
+        } catch (e: Exception) {
+            null
+        }
+
+        return null
+    }
+
+    private fun buildMovie(
+        tmdbMovie: Movie, omdbMovie: Movie ): Movie {
+        return Movie(
+            id = tmdbMovie.id,
+            title = tmdbMovie.title,
+            overview = "TMDB: ${tmdbMovie.overview}\n\n OMDB: ${omdbMovie.overview}",
+            releaseDate = tmdbMovie.releaseDate,
+            poster = tmdbMovie.poster,
+            backdrop = tmdbMovie.backdrop,
+            originalTitle = tmdbMovie.originalTitle,
+            originalLanguage = tmdbMovie.originalLanguage,
+            popularity = (tmdbMovie.popularity + omdbMovie.popularity) / 2.0,
+            voteAverage = (tmdbMovie.voteAverage + omdbMovie.voteAverage) / 2.0,
+        )
+    }
+
+}
